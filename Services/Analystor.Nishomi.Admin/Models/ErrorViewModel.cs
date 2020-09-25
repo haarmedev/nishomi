51246a68a77b49416088fc7386e827e87a1d649e
Anoop Sharma <ksharma.anoop@gmail.com>
2020-09-26 00:03:50 +0530

    Created solution setup

---
+++ Services/Analystor.Nishomi.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Analystor.Nishomi.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
