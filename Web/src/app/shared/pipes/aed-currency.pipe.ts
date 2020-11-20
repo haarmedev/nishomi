d7b9b9b777bd637827c8d2eba7f3f5ec52814d84
Sijith O S <sijith.os78@gmail.com>
2020-11-20 20:59:25 +0530

    UI issue fixes

--- Web/src/app/shared/pipes/aed-currency.pipe.ts
+++ Web/src/app/shared/pipes/aed-currency.pipe.ts
@@ -1,21 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { formatCurrency, getCurrencySymbol } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({
   name: 'aedcurrency',
+  pure: false,
 })
 export class AEDcurrencyPipe implements PipeTransform {
+  constructor(private translate: TranslateService) {}
   transform(
     value: number,
-    currencyCode: string = 'AED ',
+    currencyCode: string = 'aed',
     display: 'code' | 'symbol' | 'symbol-narrow' | string | boolean = 'symbol',
     digitsInfo: string = '0.2-2',
     locale: string = 'en-US'
   ): string | null {
+    console.log('triggered');
     return formatCurrency(
       value,
       locale,
-      getCurrencySymbol(currencyCode, 'wide'),
+      getCurrencySymbol(`${this.translate.instant(currencyCode)}`, 'wide'),
       currencyCode,
       digitsInfo
     );
