9fd40391d6fd0d477089c34b3874a0f9e5668956
fawaskallayi <fawaskallayi@gmail.com>
2020-12-22 20:40:39 +0530

    Authentication implementation

---
+++ Analystor.Nishomi.Admin/Controllers/CategoryController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Analystor.Nishomi.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Analystor.Nishomi.Admin.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        /// <summary>
+        /// The category service
+        /// </summary>
+        private readonly ICategory _categoryService;
+
+        /// <summary>
+        /// The logger
+        /// </summary>
+        private readonly ILogger<CategoryController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryController"/> class.
+        /// </summary>
+        /// <param name="categoryService">The category service.</param>
+        public CategoryController(ICategory categoryService, ILogger<CategoryController> logger)
+        {
+            this._categoryService = categoryService;
+            this._logger = logger;
+        }
+
+
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View("~/Views/Category/Index.cshtml");
+        }
+
+        /// <summary>
+        /// Creates this instance.
+        /// </summary>
+        /// <returns></returns>
+        [Microsoft.AspNetCore.Authorization.Authorize]
+        public IActionResult Create()
+        {
+            return View("~/Views/Category/Create.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryDTO category)
+        {
+            bool status = false;
+                try
+                {
+                        status = this._categoryService.Create(category);
+                        return Json(new { message = CommonConstants.SuccessfullyCreated, status = true });
+                }
+                catch (Exception ex)
+                {
+                    this._logger.LogError(ex, "Error in creating category");
+                }
+
+            return View("~/Views/Category/Index.cshtml");
+        }
+
+        /// <summary>
+        /// Edits the specified category identifier.
+        /// </summary>
+        /// <param name="categoryId">The category identifier.</param>
+        /// <returns></returns>
+        public IActionResult Edit(Guid id)
+        {
+            var item = this._categoryService.GetCategory(id);
+            return View("~/Views/Category/Edit.cshtml",item);
+        }
+
+        [HttpPost]
+        public IActionResult Update(CategoryDTO category)
+        {
+            bool status = false;
+
+                try
+                {
+                        status = this._categoryService.Update(category);
+                        return Json(new { message = CommonConstants.SuccessfullyUpdated, status = true });
+                }
+                catch (Exception ex)
+                {
+                    this._logger.LogError(ex, "Error in updating category");
+                }
+
+            return View("~/Views/Category/Index.cshtml");
+        }
+
+        /// <summary>
+        /// Deletes the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public bool Delete(Guid id)
+        {
+            bool status = false;
+            try
+            {
+                status = this._categoryService.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError(ex, "Error in category delete", ex);
+            }
+
+            return status;
+        }
+
+        /// <summary>
+        /// Gets the categorys.
+        /// </summary>
+        /// <param name="dataTable">The data table.</param>
+        /// <returns></returns>
+        public IActionResult GetCategorys(DataTableRequest dataTable)
+        {
+            var categories = this._categoryService.GetCategories(out int recordsTotal, dataTable.SearchValue, dataTable.Skip, dataTable.PageSize, dataTable.SortColumn, dataTable.SortColumnDirection);
+
+            var items = new
+            {
+                draw = dataTable.Draw,
+                recordsTotal,
+                recordsFiltered = recordsTotal,
+                data = categories
+            };
+
+            return Json(items);
+        }
+    }
+}
---
+++ Analystor.Nishomi.Admin/IdentityServer/DataSeeder.cs
@@ -0,0 +1,59 @@
+﻿using Analystor.Nishomi.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data;
+
+namespace WebApplication2.IdentityServer
+{
+    public class DataSeeder
+    {
+        public static void EnsureSeedData(string connectionString)
+        {
+            var services = new ServiceCollection();
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseMySql(connectionString));
+
+            services.AddIdentity<IdentityUser, Role>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddLogging(builder => builder.AddConsole());
+
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                    context.Database.Migrate();
+
+                    var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                    
+                    var admin = userMgr.FindByNameAsync("ahlan@nishomiabayas.com").Result;
+                    if (admin == null)
+                    {
+                        admin = new User
+                        {
+                            UserName = "ahlan@nishomiabayas.com",
+                            Email = "ahlan@nishomiabayas.com",
+                            FirstName = "Super",
+                            LastName = "Admin",
+                            EmailConfirmed = true,
+                            IsActive = true
+                        };
+                        var result = userMgr.CreateAsync(admin, "Pass123$").Result;
+                        if (!result.Succeeded)
+                        {
+                            throw new Exception(result.Errors.First().Description);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    }
